b71aa0da8aa665a12152d642b18594fba3a24525
darshannayak21 <darshan.nayak1@mitwpu.edu.in>
2026-07-23 01:50:57 +0530

    localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,11 +1,20 @@
-export let cart=[{
-  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity:2
-},
+export let cart=JSON.parse(localStorage.getItem('cart'));
+if(!cart)
 {
-  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity:1
-}];
+    cart = [{
+    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity:2
+  },
+  {
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity:1
+  }];
+}
+
+function saveCartToLocalStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
+
 export function addingtocart(productId)
 {
   let matchingitem=false;
@@ -26,6 +35,7 @@ export function addingtocart(productId)
       );
     }
     console.log(cart);
+    saveCartToLocalStorage();
   }
 
   export function removingfromcart(productId)
@@ -39,4 +49,5 @@ export function addingtocart(productId)
 
     });
     cart=newCart;
+    saveCartToLocalStorage();
   }
